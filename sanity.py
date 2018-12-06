0ddfa7890cd5ab768c56f04d193010ae974a8e58
Mark Hopkins <hopkinsm@reed.edu>
2018-12-06 14:41:56 -0800

    More refactoring.

---
+++ sanity.py
@@ -0,0 +1,107 @@
+import inspect
+
+def listfunction_names(obj):
+  '''
+  listfunction_names takes an object(e.g: module, class) and returns an array of the function names (strings)
+  ''' 
+  all_functions = inspect.getmembers(obj, inspect.isfunction)
+  names = [x[0] for x in all_functions]
+  return names
+
+
+def arg_compare(ta_obj, hw_obj, session):
+    """
+    Determines whether the arguments of two methods are the same.
+
+    """    
+
+    def getfunction_argcount(obj, function_name):
+        '''
+        getfunction_argcount returns the number of args in a function
+        '''
+        f = getattr(obj, function_name)
+        arg_count = f.__code__.co_argcount
+        return arg_count
+
+
+    log = []
+    ta_funcs = set(listfunction_names(ta_obj))
+    hw_funcs = set(listfunction_names(hw_obj))
+    ta_farg = {f:getfunction_argcount(ta_obj, f) for f in ta_funcs}
+    hw_farg = {f:getfunction_argcount(hw_obj, f) for f in hw_funcs}
+    args_there = True #innocent until proven guilty
+
+    for func, arg_count in ta_farg.items():
+      hw_arg_count = hw_farg[func]
+      if arg_count == hw_arg_count:
+        log.append("PASSED@{!s}: {!r} args defined in ta-{!s}. {!r} args in submitted-{!s}".format(func, arg_count, ta_obj.__name__ + "." + func, hw_arg_count, hw_obj.__name__ + "." + func))
+      else: 
+        log.append("ERROR@{!s}: {!r} args defined in ta-{!s}. {!r} args in submitted-{!s}".format(func, arg_count, ta_obj.__name__ + "." + func, hw_arg_count, hw_obj.__name__ + "." + func))
+        session.x_log("You defined {!r} args in submitted-{!s}, there should be {!r} argument(s)".format(hw_arg_count, hw_obj.__name__ + "." + func, arg_count))
+        args_there = False
+    return args_there, log
+
+
+def compare(ta_module, hw_module, session):
+    """
+     compare() does a basic comparison of two modules (or objects), checking to make sure that all functions and classes
+     are defined with the appropriate number of arguments. returns true if so, false otherwise.
+    
+    """ 
+    def getclass_dict(module):
+        '''
+        getclass_dict takes a module and returns a dict of {(className:classObject), (...)}
+        ''' 
+        all_classes = inspect.getmembers(module, inspect.isclass)
+        classes = {x[0]:x[1] for x in all_classes}
+        return classes
+
+
+
+
+    all_there = True #innocent until proven guilty
+
+    #scrape functions from top level:
+    ta_top_lvl = set(listfunction_names(ta_module))
+    hw_top_lvl = set(listfunction_names(hw_module))
+    if ta_top_lvl <= hw_top_lvl:
+      all_there, log = arg_compare(ta_module, hw_module, session)
+    else:
+      #also log the missing ones
+      missing = ta_top_lvl - hw_top_lvl
+      missing_str = ", ".join(str(e) for e in missing)
+      session.x_log("{!s}.py is missing the following functions: {!s}".format(hw_module.__name__, missing_str))
+      all_there = False
+
+    #scrape classes from TA_file, hw_file, and compare them
+    hw_class_dict = getclass_dict(hw_module)
+    ta_class_dict = getclass_dict(ta_module)
+    ta_class_names = set(ta_class_dict.keys())
+    hw_class_names = set(hw_class_dict.keys())
+    common_class_names = ta_class_names & hw_class_names
+
+    #high level check to make sure all the classes in TA_module are defined in hw_module (no check to see if there's extra stuff defined in hw_module)
+    if ta_class_names > hw_class_names:
+      missing = ta_class_names - hw_class_names
+      missing_str = ", ".join(str(e) for e in missing)
+      session.x_log("{!s}.py is missing the following classes: {!s}".format(hw_module.__name__, missing_str))
+      all_there = False
+
+    #scrape functions from the common classes:
+    for cls_name in common_class_names:
+      hw_cls = hw_class_dict[cls_name]
+      ta_cls = ta_class_dict[cls_name]
+      ta_cls_funcs = set(listfunction_names(ta_cls))
+      hw_cls_funcs = set(listfunction_names(hw_cls))
+      if ta_cls_funcs <= hw_cls_funcs:
+        cls_args_there, log = arg_compare(ta_cls, hw_cls)
+        if all_there and not cls_args_there:
+          all_there = False
+      else:
+        missing = ta_cls_funcs - hw_cls_funcs
+        missing_str = ", ".join(str(e) for e in missing)
+        session.x_log("Your class {!s} is missing some functions: {!s}".format(hw_module.__name__ +"." + cls_name, missing_str))
+        all_there = False
+
+    return all_there# -*- coding: utf-8 -*-
+
---
+++ session.py
@@ -0,0 +1,137 @@
+import json
+import datetime
+import inspect
+
+
+class Session():
+  '''
+  Session object: a session object takes in two modules (a key, or solution module, and a submission module)
+  it stores the results of the compare() function and maintains various logs and score information
+  '''
+  def __init__(self, ta_obj=None, hw_obj=None):
+    self.start_time = datetime.datetime.now().strftime(
+            "%A, %d. %B %Y %I:%M:%S%p")
+    if ta_obj:
+      self.ta_obj = ta_obj
+    if hw_obj:
+      self.hw_obj = hw_obj
+    self.DATA_DIR = ''
+    self.DATA_FILE = 'data.json'
+    self.log = {"info":{}, 
+                "internal_log":[], 
+                "external_log":[], 
+                "score_sum": None, 
+                "max_score": None}
+    submission_data = self.DATA_DIR + self.DATA_FILE
+    try:
+        with open(submission_data) as data_file: 
+            self.data = json.load(data_file)
+    except FileNotFoundError:
+        self.data = { "attempts": 1, "prevscore": 0, "timedelta": 0 }
+
+
+  def get_attempts(self):
+    '''
+    get_attempts returns the number of attempts on this problem / problem set
+    '''
+    attempts = self.data['attempts']
+    return attempts
+
+  def get_timedelta(self):
+    '''
+    gettimedelta returns the 0(on time) or -1 (late) on this 
+    problem / problem set, just a placeholder for now
+    
+    '''
+    timedelta = self.data['timedelta']
+    return timedelta
+
+  def get_prevscore(self):
+    '''
+    get_prevscore returns the previous score (if any, one this problem)
+    '''
+    prevscore = self.data['prevscore']
+    return prevscore
+
+  def _info(self, key, message):
+    '''
+    used internally: _info logs to the 'info' key of the session log, 
+    contains data about the session object
+    '''
+    self.log["info"][key] = message
+
+ 
+  def i_log(self, message):
+    '''
+    for external use: logs to the internal portion of the session log, 
+    anything logged here is saved to the database for the TA's reference
+    '''
+    self.log["internal_log"].append(message)
+
+
+  def x_log(self, message):
+    '''
+    for external use: logs to the external portion of the session log,
+    anything logged here is passed back to the student
+    '''
+    self.log["external_log"].append(message)
+
+  def set_max_score(self, max_score):
+    '''sets the max score for the problem'''
+    self._maxscore = max_score
+
+  def set_score(self, new_score):
+    ''' sets the new score for the homework '''
+    self._score = new_score
+
+  def get_score(self):
+    '''returns the current score'''
+    return self._score
+
+  def update_score(self, score_mod):
+    ''' changes the value of self._score by the (int) given '''
+    self._score += score_mod
+
+  def finalize(self):
+    '''
+    for external use: finalize computes the final score (using the 
+    values of the score key) and logs the start & end times of the session, 
+    along with some data about the problem set
+    finally, it dumps the log into a json object
+    
+    '''
+    self.log["score_sum"] = self._score
+    self.log["max_score"] = self._maxscore
+    self.end_time = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M:%S%p")
+    self._info("start_time", self.start_time)
+    self._info("end_time", self.end_time)
+    self._info("timedelta", self.get_timedelta())
+    self._info("attempts", self.get_attempts())
+    self._info("final score", self._score)
+    self._info("max score", self._maxscore)
+
+    print(json.dumps(self.log))
+    return json.dumps(self.log)
+
+    def find_tabs(module):
+      #function to check if a module contains tab characters
+        source = inspect.getsource(module)
+        return '\t' in source
+
+    def tab_report(self):
+        if find_tabs(self.hw_obj):
+            self.x_log("It looks like you are using tabs in your source code." +
+                     "You should configure your editor so it changes tabs to spaces.")
+
+    
+class DeadSession(Session):
+
+    def __init__(self):
+        Session.__init__(self, None, None)
+
+    def compare(self):
+        return True
+    
+    def test_hw_function(self, name, pub_inputs, priv_inputs, the_same=(lambda x,y: x == y)):
+        return False
+
