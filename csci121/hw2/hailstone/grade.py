a65c6935acce17d8cf9fcbe7a6ffa12e8a1e5e60
hopkinsm <hopkinsm@C02X456YJHD3.local>
2018-12-09 20:17:37 -0800

    Removed hw2.

--- csci121/hw2/hailstone/grade.py
+++
@@ -1,5 +0,0 @@
-from autograding import FunctionStdoutTest
-problem = FunctionStdoutTest("ta_hailstone", "hailstone", "hailstone")
-pub_inputs = [(7,), (13,)]
-priv_inputs = [(5000,), (12345678,), (1,)] 
-problem.batch_test(pub_inputs, priv_inputs)
